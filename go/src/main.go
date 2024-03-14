f6bb8d34503d87ad1ced09a2fd80397b96cf0757
Daniel Rocha <daniel.rochats@gmail.com>
2024-03-14 04:20:54 -0300

    update

--- go/src/main.go
+++ go/src/main.go
@@ -161,6 +161,57 @@ func main() {
 
 		return nil // retorno nil para indicar sucesso na resposta
 	})
+	app.Post("/kickUser", func(c *fiber.Ctx) error {
+		// Parse dos dados do corpo da requisição
+		var requestData struct {
+			Username string `json:"username"`
+			RoomName string `json:"roomname"`
+		}
+		if err := c.BodyParser(&requestData); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Failed to parse request body",
+			})
+		}
+	
+		// Verifica se a sala de bate-papo existe
+		chatroom, exists := chatrooms[requestData.RoomName]
+		if !exists {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Chatroom not found",
+			})
+		}
+	
+		// Remove o usuário da sala de bate-papo
+		var updatedUsers []string
+		for _, user := range chatroom.Users {
+			if user != requestData.Username {
+				updatedUsers = append(updatedUsers, user)
+			}
+		}
+		chatroom.Users = updatedUsers
+	
+		// Envia uma mensagem informando aos clientes WebSocket sobre a remoção do usuário
+		userJSON, err := json.Marshal(map[string]interface{}{
+			"type":     "removeUser",
+			"user":     requestData.Username,
+			"chatRoom": requestData.RoomName,
+		})
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Failed to serialize user data",
+			})
+		}
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, userJSON)
+			if err != nil {
+				log.Println("Erro ao enviar mensagem para o cliente WebSocket:", err)
+				continue
+			}
+		}
+	
+		// Retorna uma resposta indicando sucesso
+		return c.SendStatus(fiber.StatusOK)
+	})
 
 	// Rota para enviar mensagem
 	app.Post("/sender", func(c *fiber.Ctx) error {
