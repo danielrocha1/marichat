0621a5c0c7d3a22b5e4588ca13ba2e2e20fb5255
Daniel Rocha <daniel.rochats@gmail.com>
2024-03-26 21:25:16 -0300

    login

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './index.css';
 
 const Login = ({ handleSubmit, setUser, setChatroomName, user, chatroomName }) => {
   return (
