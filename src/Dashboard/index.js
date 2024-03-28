3923d94c286459ff4738f14f9070c5812e923e3e
Daniel Rocha <daniel.rochats@gmail.com>
2024-03-28 22:07:20 +0000

    web

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -3,10 +3,10 @@ import ChatContext from '../ChatContext';
 import './index.css';
 
 // Componentes
-const Sidebar = ({ user }) => {
+const Sidebar = ({ user, chats }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const toggleSidebar = () => {
+  const toggleSidebar = ({chats}) => {
     console.log(user.data.hostid)
     console.log(chats)
     setIsSidebarOpen(!isSidebarOpen);
@@ -118,7 +118,7 @@ const Dashboard = () => {
     <div>
       <TopHeader handleLogout={handleLogout} />
       <div className="app">
-        <Sidebar user={userData} />
+        <Sidebar user={userData} chats={chats}  />
         <div className="container">
           <div className="createChat">
             <button onClick={() => console.log("criar chat")} className="violet-button">Criar Chat</button>
