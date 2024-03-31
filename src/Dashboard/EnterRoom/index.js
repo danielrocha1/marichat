7dc056e9b0a19e34897f2bfce74724fa043f3402
Daniel Rocha <daniel.rochats@gmail.com>
2024-03-30 22:02:47 -0300

    dsda

--- src/Dashboard/EnterRoom/index.js
+++ src/Dashboard/EnterRoom/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import './index.css'; // Arquivo de estilo CSS para centralizar o formulário
 import { useNavigate } from 'react-router-dom';
 
