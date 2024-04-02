805b0a7d1b49fe46950f484f57db566f0805a495
Daniel Rocha <daniel.rochats@gmail.com>
2024-04-01 21:09:13 -0300

    asd

--- src/ChatRoom/index.js
+++ src/ChatRoom/index.js
@@ -127,7 +127,7 @@ function ChatRoom({ children }) {
         setMessages(prevMessages => [...prevMessages, Message]);
       }
 
-      if (message.Type === 'receiver' && message.Name === userData.user && message.upload === true) {
+      if (message.Type === 'receiver' && message.HostID === userData.data.hostid && message.upload === true && message.ChatID === chat.chatid) {
         if (message.Label === 'image/png' || message.Label === 'image/jpg' || message.Label === 'image/jpeg') {
           const Message = (<SenderImage imageData={message.Message} Hour={message.Timestamp} />);
           setMessages(prevMessages => [...prevMessages, Message]);
