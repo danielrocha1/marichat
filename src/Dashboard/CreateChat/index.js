d43e483cd588bab81d9f9eea14117e510d534f4f
Daniel Rocha <daniel.rochats@gmail.com>
2024-04-13 08:29:15 -0300

    asd

---
+++ src/Dashboard/CreateChat/index.js
@@ -0,0 +1,89 @@
+import React, { useState, useContext } from 'react';
+import './index.css'; // Arquivo de estilo CSS para centralizar o formulário
+import { useNavigate } from 'react-router-dom';
+import ChatContext from '../../ChatContext';
+
+function CreateChat() {
+  const { userData, setUserData} = useContext(ChatContext);
+  
+  const [chatRoom, setChatroomName] = useState('');
+  const [chatid, setChatroomName] = useState('');
+  const [private, setChatroomName] = useState('');
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const navigate = useNavigate();
+  
+ 
+  
+  const handleSubmit = async (e) => {
+
+    const queryString = new URLSearchParams(chat,userData).toString();  
+
+    e.preventDefault();
+      try {
+        const response = await fetch('https://marichat-go.onrender.com/addUser', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ 
+            username: userData.data.username,
+            hostid: userData.data.hostid,
+            chatid: chat.chatid 
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error('Erro ao enviar os dados');
+        }
+
+        navigate(`/chatroom?${queryString}`);
+      } catch (error) {
+        console.error('Erro:', error.message);
+      }
+  };
+
+  return (
+    <div>
+      {/* Botão para abrir o modal */}
+      <button onClick={() => setModalIsOpen(!modalIsOpen)} className="violet-button">Entrar na Sala</button>
+
+      {/* Modal */}
+      {modalIsOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <form onSubmit={handleSubmit} className="form">
+              <h2>Entrar na sala</h2>
+              <div className="form-group">
+                <label htmlFor="user">Nome da Sala:</label>
+                <input
+                  type="text"
+                  id="chatroomName"
+                  value={chatRoom}
+                  onChange={(e) =>
+                    setChatroomName(e.target.value)
+                    }
+                />
+                <label htmlFor="user">ID da Sala:</label>
+                <input
+                  type="text"
+                  id="id"
+                  value={chatRoom}
+                />
+                <label htmlFor="user">Chat Privado ou Público:</label>
+                <input
+                  type="radio"
+                  id="private"
+                  value={chatRoom}
+                />
+              </div>
+              <button type="submit">Entrar na Sala de Chat</button>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default CreateChat;
