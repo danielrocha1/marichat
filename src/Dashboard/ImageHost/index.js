2840baa90086dbb2c45aedc9a6ed44c0c4656428
Daniel Rocha <daniel.rochats@gmail.com>
2024-04-17 19:56:59 -0300

    asd

--- src/Dashboard/ImageHost/index.js
+++ src/Dashboard/ImageHost/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css"; // Importe o arquivo CSS para estilização
 
 import photo from "./av.png";
@@ -7,6 +7,7 @@ const ImageHost = ({ user }) => {
   const [image, setImage] = useState(photo);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [formData, setFormData] = useState(new FormData());
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -16,37 +17,41 @@ const ImageHost = ({ user }) => {
         setImage(reader.result);
       };
       reader.readAsDataURL(file);
-      uploadImage(file);
+
+      const newFormData = new FormData();
+      newFormData.append("photo", file);
+      newFormData.append("hostid", user.data.hostid);
+      setFormData(newFormData);
     }
   };
 
-  const uploadImage = async (file) => {
-    setIsLoading(true);
-    setError(null);
+  useEffect(() => {
+    const uploadData = async () => {
+      if (formData.get("photo")) {
+        setIsLoading(true);
+        setError(null);
 
-    const formData = new FormData();
-    formData.append("photo", file);
-    formData.append("hostid", user.data.hostid);
-  
-    try {
-      const response = await fetch("https://marichat-go.onrender.com/upload-photo", {
-        method: "POST",
-        body: formData,
-      });
-      
-      console.log(formData)
+        try {
+          const response = await fetch("https://marichat-go.onrender.com/upload-photo", {
+            method: "POST",
+            body: formData,
+          });
 
-      if (response.ok) {
-        console.log("Imagem enviada com sucesso!");
-      } else {
-        throw new Error("Falha ao enviar imagem.");
+          if (response.ok) {
+            console.log("Imagem enviada com sucesso!");
+          } else {
+            throw new Error("Falha ao enviar imagem.");
+          }
+        } catch (error) {
+          setError(error.message);
+        } finally {
+          setIsLoading(false);
+        }
       }
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+    };
+
+    uploadData();
+  }, [formData, user.data.hostid]);
 
   return (
     <div className="">
