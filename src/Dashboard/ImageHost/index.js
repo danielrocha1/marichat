c6c8124f64939e2a790733404c57366f158ad490
Daniel Rocha <daniel.rochats@gmail.com>
2024-04-17 20:46:43 -0300

    fsgg

--- src/Dashboard/ImageHost/index.js
+++ src/Dashboard/ImageHost/index.js
@@ -7,7 +7,7 @@ const ImageHost = ({ user }) => {
   const [image, setImage] = useState(photo);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [formData, setFormData] = useState(new FormData());
+  
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -17,26 +17,23 @@ const ImageHost = ({ user }) => {
         setImage(reader.result);
       };
       reader.readAsDataURL(file);
-
-      const newFormData = new FormData();
-      newFormData.append("photo", file);
-      newFormData.append("hostid", user.data.hostid);
-      setFormData(newFormData);
     }
   };
 
   useEffect(() => {
-    console.log(formData.get("photo"))
     
+
     const uploadData = async () => {
-      if (formData.get("photo")) {
         setIsLoading(true);
         setError(null);
 
         try {
           const response = await fetch("https://marichat-go.onrender.com/upload-photo", {
             method: "POST",
-            body: formData,
+            body: JSON.stringify({
+              hostid:user.data.hostid,
+              photo:image,
+            }),
           });
 
           if (response.ok) {
@@ -49,7 +46,6 @@ const ImageHost = ({ user }) => {
         } finally {
           setIsLoading(false);
         }
-      }
     };
 
     uploadData();
