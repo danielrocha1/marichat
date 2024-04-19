8c079ffa8b13292a7cb9c9496088289de4630743
Daniel Rocha <daniel.rochats@gmail.com>
2024-04-19 20:50:44 -0300

    uyf

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -153,11 +153,13 @@ const Dashboard = () => {
       <TopHeader handleLogout={handleLogout} />
       <div className="app">
         <Sidebar user={userData} chats={chats} />
-        <div className="container">
-          <div className="createChat">
+        <div className="createChat">
               <EnterRoom />
               <CreateChat />
           </div>
+          
+        <div className="container">
+         
 
           <div className="">
             <ChatTable userData={userData} setChats={setChats} chats={chats} /> {/* passa um array vazio para chats */}
