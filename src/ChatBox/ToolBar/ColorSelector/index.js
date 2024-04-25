9a50fa30b1b89018597b8ccb6a7261103c83579f
Daniel Rocha <daniel.rochats@gmail.com>
2024-04-25 17:28:53 -0300

    asd

--- src/ChatBox/ToolBar/ColorSelector/index.js
+++ src/ChatBox/ToolBar/ColorSelector/index.js
@@ -4,18 +4,19 @@ import { HexColorPicker } from 'react-colorful';
 
 const ColorOptions = ({ onSelectColor, colors, type }) => {
   const [hexColorVisible, setHexColorVisible] = useState(false);
-  const [selectedColor, setSelectedColor] = useState('');
-  const [hexColor, setHexColor] = useState('black');
   const [selectedOptionIndex, setSelectedOptionIndex] = useState(null);
+  const [selectedColor, setSelectedColor] = useState('');
 
-  
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+  const handleSelectColor = (color, index) => {
+    setSelectedColor(color);
+    setSelectedOptionIndex(index);
+    onSelectColor(color, type);
+    setHexColorVisible(false);
   };
+
   const toggleHexColor = () => {
     setHexColorVisible(!hexColorVisible);
-    setSelectedOptionIndex(null)
+    setSelectedOptionIndex(null);
   };
 
   return (
@@ -26,34 +27,23 @@ const ColorOptions = ({ onSelectColor, colors, type }) => {
             key={index}
             className={`color-option ${selectedOptionIndex === index ? 'selected' : ''}`}
             style={{ background: color }}
-            onClick={() => {
-              setSelectedColor(color);
-              setSelectedOptionIndex(index);
-              onSelectColor(color, type);
-              setHexColorVisible(false); // Definir hexColorVisible como false ao selecionar uma cor sólida
-            }}
+            onClick={() => handleSelectColor(color, index)}
           ></div>
         ))}
         <div
-          className={`color-option ${hexColorVisible ? 'selected' : ''}`} // Aplicar a classe 'selected' se o seletor de cores hexadecimal estiver visível
-          style={{ backgroundColor: hexColor, color: "white" }}
+          className={`color-option ${hexColorVisible ? 'selected' : ''}`}
+          style={{ backgroundColor: selectedColor, color: 'white' }}
           onClick={toggleHexColor}
         >
-          <p
-            style={{ fontSize: "10px", fontWeight: "bold", color: "white" }}
-          >
-            ?
-          </p>
+          <p style={{ fontSize: '10px', fontWeight: 'bold', color: 'white' }}>?</p>
         </div>
         {hexColorVisible && (
           <div className="hex-color-picker-container">
             <HexColorPicker
               color={selectedColor}
-              onChange={color => {
-                onSelectColor(color, type);
+              onChange={(color) => {
                 setSelectedColor(color);
-              setHexColor(color);
-
+                handleSelectColor(color);
               }}
             />
           </div>
@@ -61,69 +51,42 @@ const ColorOptions = ({ onSelectColor, colors, type }) => {
       </div>
     </div>
   );
-}
+};
 
 const ColorSelector = ({ isOpen, onClose, onSelectColor }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const background = [
-    "linear-gradient(#98c15c,#80bf4d,#64b231,#1f930f,#107b18)",
-    "linear-gradient(#f0a1a0,#b30f15,#96090f,#850606,#63080c)",
-    "linear-gradient(#385a7c,#f97171,#f99192,#8ad6cc,#b2eee6)",
-    "linear-gradient(#360b19,#673844,#9c6874,#d49ca8,#ffd8e0)"
+    'linear-gradient(#98c15c,#80bf4d,#64b231,#1f930f,#107b18)',
+    'linear-gradient(#f0a1a0,#b30f15,#96090f,#850606,#63080c)',
+    'linear-gradient(#385a7c,#f97171,#f99192,#8ad6cc,#b2eee6)',
+    'linear-gradient(#360b19,#673844,#9c6874,#d49ca8,#ffd8e0)',
   ];
 
-  const chatBoxColor = [
-    "#80bf4d",
-    "#96090f",
-    "#f97171",
-    "#673844"
-  ];
+  const chatBoxColor = ['#80bf4d', '#96090f', '#f97171', '#673844'];
 
-  const chatBorderColor = [
-    "black",
-    "white"    
-  ];
-  
-  const receiverColor = [
-    "black",
-    "white"    
-  ];
+  const chatBorderColor = ['black', 'white'];
 
-  const senderColor = [
-    "black",
-    "white"    
-  ];
+  const receiverColor = ['black', 'white'];
+
+  const senderColor = ['black', 'white'];
 
   if (!isOpen) return null;
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-      <div >
+    <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+      <div>
+        <p style={{ color: 'black', fontSize: '12px' }}>Selecione a cor do fundo:</p>
+        <ColorOptions onSelectColor={onSelectColor} colors={background} type="fundo" />
 
-        <p style={{ color: "black", fontSize: "12px" }}>Selecione a cor do fundo:</p>
-          <ColorOptions
-            onSelectColor={onSelectColor}
-            colors={background}
-            type="fundo"
-          />
+        <p style={{ color: 'black', fontSize: '12px' }}>Selecione a cor do chat:</p>
+        <ColorOptions onSelectColor={onSelectColor} colors={chatBoxColor} type="chat" />
 
-
-        <p style={{ color: "black", fontSize: "12px" }}>Selecione a cor do chat:</p>
-        <ColorOptions
-          onSelectColor={onSelectColor}
-          colors={chatBoxColor}
-          type="chat"
-        />
-
-     
-
-        <p style={{ color: "black", fontSize: "12px" }}>Selecione a cor das bordas:</p>
-        <ColorOptions
-          onSelectColor={onSelectColor}
-          colors={chatBorderColor}
-          type="bordas"
-        />
-
+        <p style={{ color: 'black', fontSize: '12px' }}>Selecione a cor das bordas:</p>
+        <ColorOptions onSelectColor={onSelectColor} colors={chatBorderColor} type="bordas" />
       </div>
     </div>
   );
