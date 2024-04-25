b9facb3b7bd9afa7cbc7fe1bccfba3510969c189
Daniel Rocha <daniel.rochats@gmail.com>
2024-04-25 18:21:25 -0300

    asdddd

--- src/ChatRoom/index.js
+++ src/ChatRoom/index.js
@@ -23,31 +23,6 @@ function ReceiverMessage(props) {
   );
 }
 
-const Sidebar = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
-  return (
-    <div className={`sidebarChat ${isSidebarOpen ? 'open' : ''}`}>
-      <div>
-        <div onClick={toggleSidebar} className="toggle-btn">
-          <div className="bar1"></div>
-          <div className="bar2"></div>
-          <div className="bar3"></div>
-        </div>
-      </div>
-      <div className="user-info">
-        <p>Email: </p>
-      </div>
-      
-    </div>
-  );
-};
-
-
 function SenderMessage(props) {
   const timestamp = new Date();
   const hora = format(timestamp, 'HH:mm');
