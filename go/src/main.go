c8f0fea7cc2bca4261e408b0a6ba87b9605fdb32
Daniel Rocha <daniel.rochats@gmail.com>
2024-07-24 17:46:45 -0300

    ff

--- go/src/main.go
+++ go/src/main.go
@@ -188,7 +188,7 @@ func main() {
 		fmt.Println(string(registerReqJSON))
 		
 		// Executar a consulta SQL para inserir os dados do usuário na tabela UserInfo
-		_, err := db.Exec("INSERT INTO userinfo (hostid, fullname, username, email, password, birthdate) VALUES ($1, $2, $3, $4, $5, $6)",
+		_, err = db.Exec("INSERT INTO userinfo (hostid, fullname, username, email, password, birthdate) VALUES ($1, $2, $3, $4, $5, $6)",
 			registerReq.HostID, registerReq.FullName, registerReq.Username, registerReq.Email, registerReq.Password, registerReq.Birthdate)
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
