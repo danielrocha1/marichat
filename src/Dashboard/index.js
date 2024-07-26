2ca77e18762d0d9ad83051cef41790ec54e8370e
Daniel Rocha <daniel.rochats@gmail.com>
2024-07-25 22:04:57 -0300

    asdd

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -117,8 +117,6 @@ const navigate = useNavigate();
         console.error('Erro:', error.message);
       }
     };
-
-    addUserToChat();
   };
 
   const removeChat = async (chat, userData) => { 
