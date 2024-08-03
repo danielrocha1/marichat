c7ee84ce33881df20ab3c96177697f502ab91a14
Daniel Rocha <daniel.rochats@gmail.com>
2024-08-03 03:15:50 -0300

    ghj

--- src/GuestInfo/index.js
+++ src/GuestInfo/index.js
@@ -90,13 +90,13 @@ const GuestInfo = (props) => {
 
   return (
     <div id={`${props.hostid}`} className="box">
-      {props.hostid === userData.data.hostid && (
+      
         <div className="kick" onClick={openModal}>
           <div className="Bar1"></div>
           <div className="Bar2"></div>
           <div className="Bar3"></div>
         </div>
-      )}
+      
       <div className={`guestBox ${isTyping ? 'typing' : ''}`}>
         <img src={props.photo} className="guestPhoto" alt="logo" />
         <div className="guestInfo">
