fb0f8580c1e637a1f0447416d13a825f397bf722
Daniel Rocha <daniel.rochats@gmail.com>
2024-08-06 22:41:12 -0300

    asd

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -97,7 +97,7 @@ const friends = [
 
 const FriendList = () => {
   return (
-    <div className="friends0">
+    <div className="friends">
       <h5 style={{color:"green", textAlign: "center" }}>Amigos online</h5>
       <div className="friend-list0">
       {friends.map(friend => (
